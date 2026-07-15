cc3f1c37654fe9a28c6fad9ad5c3103ace1de82b
AndrewSBukin <andrewsbukin@gmail.com>
2026-07-16 02:25:08 +0300

    ChatFolders

---
+++ Migrations/20260715232437_V19_2-ChatFolders.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IDMChat.Migrations
+{
+    /// <inheritdoc />
+    public partial class V19_2ChatFolders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PinnedOrder",
+                table: "ConversationMembers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PinnedOrder",
+                table: "ConversationMembers");
+        }
+    }
+}
